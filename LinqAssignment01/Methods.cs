d5cb329ebe99fa95125bf1808abdfd96bc20b9f5
abdallaelkhateeb <119896180+abdallaelkhateeb@users.noreply.github.com>
2025-08-09 21:02:10 +0300

    Add project files.

---
+++ LinqAssignment01/Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAssignment01
+{
+    internal static class Methods
+    {
+        public static void print(this object obj)
+        {
+            Console.WriteLine(obj);
+        }
+        public static void print<T>(this IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ LinqAssignment01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LinqAssignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListGenerators.ProductList.print();
+
+        }
+    }
+}
